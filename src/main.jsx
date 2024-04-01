0445fb2356fc59a03f658822d5e753af2d2a3c5f
Juan Carrasquer <juancarrasquer@gmail.com>
2024-04-01 20:37:36 +0200

    todo

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import TagManager from "react-gtm-module";
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import {BrowserRouter} from 'react-router-dom'
-const tagManagerArgs = {
-  gtmId: 'GTM-522F863B'
-  };
-  TagManager.initialize(tagManagerArgs);
 
+ 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ChakraProvider>
